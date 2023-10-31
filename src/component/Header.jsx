8d8e12c8581561f8253237e1fdc69ed9f2798566
AbubakarImam <salamabubakarimam@gmail.com>
2023-10-31 22:10:50 +0100

    aligning

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.svg'
 function Header() {
     return (
         <div>
-            <nav className='flex items-center justify-between py-2 mb-6'>
+            <nav className='flex items-center justify-between py-2 mb-14'>
                 <img src={logo} alt="" />
                 <button className='text-black bg-white border-blue-500 border py-2 px-5 rounded-lg'>Login</button>
             </nav>
